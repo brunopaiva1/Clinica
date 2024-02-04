2e0b5a8b611e5ee525057c7297f9fd85b5203ed6
brunopaiva1 <brunopaivamaster10@gmail.com>
2024-02-04 15:01:23 -0300

    estrutura MVT

--- mysite/user/api/views.py
+++
@@ -1,9 +0,0 @@
-# views.py
-
-from rest_framework import generics
-from user.models import Item
-from user.api.serializers import ItemSerializer
-
-class ItemListCreateView(generics.ListCreateAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
---
+++ user/api/serializers.py
@@ -0,0 +1,9 @@
+# # serializers.py
+
+# from rest_framework import serializers
+# from user.models import Item
+
+# class ItemSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Item
+#         fields = '__all__'
